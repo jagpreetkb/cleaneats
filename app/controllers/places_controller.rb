7d0e3a083936b6111cdf8714d536177580520f02
jagpreetkb <jagpreetbhandaal@gmail.com>
2019-06-01 11:13:50 +0000

    added linked pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,9 +1,30 @@
 class PlacesController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
   def index
     @places = Place.all
   end
 
   def new
     @place = Place.new
+  end
+
+  def create
+    current_user.places.create(place_params)
+    redirect_to root_path
+  end
+
+  def show
+    @place = Place.find(params[:id])
+  end
+
+  def edit
+    @place = Place.find(params[:id])
+  end
+
+  private
+
+  def place_params
+    params.require(:place).permit(:name, :description, :address)
   end
 end
